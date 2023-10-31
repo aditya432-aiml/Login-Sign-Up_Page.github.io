343de77128f18274a358315a920d944249c219d6
Aditya Bhagwat <adityabhagwat432@gmail.com>
2023-10-31 14:29:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 alert('Welcome To Login/Sign Up Page !!');
+function myFunction() {
+    var emailInput = document.getElementById("email");
+    var passwordInput = document.getElementById("password");
+
+    if (emailInput.value === "admin@gmail.com" && passwordInput.value === "admin@123") {
+        alert('Welcome back');
+    } else {
+        alert('Wrong Email or Password');
+      }
+}
